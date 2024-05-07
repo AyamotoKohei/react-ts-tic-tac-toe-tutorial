d08100fc8626bceb64bd2204a7deed7728e9c53d
AyamotoKohei <koh14taka2@gmail.com>
2024-05-07 17:46:38 +0900

    コードを整理

--- src/Game.tsx
+++ src/Game.tsx
@@ -2,27 +2,25 @@ import Board from "./Board";
 import { useState } from "react";
 
 const Game = () => {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill("")]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares: string[] = history[currentMove];
 
   const handlePlay = (nextSquares: string[]) => {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
   };
 
   const jumpTo = (nextMove: number) => {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   };
 
   const moves = history.map((squares: string[], move: number) => {
     const description = move > 0 ? "Go to move #" + move : "Go to game start";
     return (
-      <li>
+      <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
